9e289c9fab99ff39c9683afae72595875859ba2f
shanlihou <shanlihou@gmail.com>
2020-07-01 20:18:48 +0800

    up

--- clipToCopy/djangoTest.js
+++ clipToCopy/djangoTest.js
@@ -1,42 +1,78 @@
-var bTest = document.getElementById("test");
-bTest.onclick = function(){
-    var url = 'path=1234';
-    var xhrUrl = "http://127.0.0.1:8000/download/";
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-        if (xhr.readyState==4)
-        {// 4 = "loaded"
-            if (xhr.status==200)
-            {// 200 = "OK"
-                console.log(xhr.responseText); 
-            }
-            else
-            {
-                console.log('failed');
-            }
+let bTest = document.getElementById("test");
+let g_cb = {};
+
+function getRpcId(func) {
+    for (let i = 0; i < 10000; i++) {
+        if (!(i in g_cb)) {
+            g_cb[i] = func;
+            return i;
         }
-    }; // Implemented elsewhere.
-    xhr.open("POST", xhrUrl, true);
-    xhr.send(url);
-}
-var getPage = function(url)
-{
-    var urlList = url.split('/');
-    console.log(urlList[urlList.length - 1]);
-    page = urlList[urlList.length - 1];
-    page = /\d+/.exec(page);
-    if (page)
-    {
-        page = page[0];
     }
-    else
-        page = "0";
-    hua = urlList[urlList.length - 2];
-    return {"hua":hua, "page":page};
+    return null;
 }
-var notuse = function()
-{
-    getPage('http://manhua.fzdm.com/131/92/index_42.html');
-    var ret = getPage('http://manhua.fzdm.com/131/93/');
-    console.log(ret);
+
+function popRpcId(rpcId) {
+    let func = g_cb[rpcId];
+    delete g_cb[rpcId];
+    return func;
+}
+
+function rpc_call() {
+    let eventObj = {};
+    eventObj['func'] = arguments[0];
+    args = [].slice.call(arguments);
+    eventObj['arg'] = args.slice(1, -1);
+    respfunc = args[args.length - 1];
+    
+    let rpcId = getRpcId(respfunc);
+    eventObj['rpcId'] = rpcId;
+    console.log("arg:", arguments, eventObj);
+    
+    chrome.tabs.query(
+        {active: true, currentWindow: true},
+        function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, eventObj, function(retObj){
+                let func = popRpcId(retObj.rpcId);
+                func.apply(null, retObj.args);
+            });
+        }
+    );
+}
+
+function getElementByAttrAll(tag, attr, value){
+    var elements = document.getElementsByTagName(tag);
+    var retArray = new Array();
+    for (var i = 0; i < elements.length; i++){
+        console.log(i, elements[i].getAttribute(attr), value);
+        if (elements[i].getAttribute(attr) == value)
+        {
+            elements[i];
+            retArray.push(elements[i]);
+        }
+    }
+    return retArray;
+}
+
+let checkBoxes = getElementByAttrAll('input', 'type', 'checkbox');
+checkBoxes.map(function (cb) {
+    (function (locCb) {
+        cb.onclick = function() {
+            console.log(locCb.id, locCb.checked);
+            rpc_call('setFilter', locCb.id, locCb.checked, (ret)=> {
+                
+            });
+        }
+        console.log('before get:', locCb.id);
+        rpc_call('getLocStorage', locCb.id, function(name, value){
+            console.log('get loc:', locCb.id, name, value);
+            locCb.checked = value == 'true' ? true: false;
+        });
+    })(cb);
+    
+});
+
+bTest.onclick = function(){
+    rpc_call('test', 1, 3, 4, (ret)=> {
+        console.log("im in resp", ret, arguments);
+    });
 }
--- clipToCopy/tapd.js
+++ clipToCopy/tapd.js
@@ -4,6 +4,7 @@ function Insert(){
     this.urlAdd = false;
     this.isReply = false;
 }
+
 function getElementByAttr(tag, attr, value){
     var elements = document.getElementsByTagName(tag);
     for (var i = 0; i < elements.length; i++){
@@ -26,19 +27,6 @@ function getElementByAttrAll(tag, attr, value){
     }
     return retArray;
 }
-function getTitle(){
-    var elements = document.getElementsByTagName("title");
-    for (var i = 0; i < elements.length; i++)
-    {
-        return elements[i].innerHTML;
-    }
-}
-function login(username, pwd){
-    document.getElementById("login_user").value = username;
-    document.getElementById("login_psw").value = pwd;
-    var btLogin = this.getElementByAttr('a', 't', 'w_Login');
-    btLogin.click();
-}
 function appendButton(name, url){
     var button = document.createElement("a");
     button.style.height = "50px";
@@ -58,71 +46,44 @@ function appendButton(name, url){
     }
     return button;
 }
-function addButton(){
-    var div = this.getElementByAttr('div', 'class', 'ui-button-box login-btnbox');
-    if (!this.buttonAdd)
-    {
-        this.buttonAdd = true;
-        div.appendChild(this.appendButton("admin123"));
-        div.appendChild(this.appendButton("Admin12("));
-        div.appendChild(this.appendButton("admin"));
-    }
-}
-function inputPwd(){
-    var pwd1 = this.getElementByAttrAll('input', 'class', 'fn-mart2 u-input');
-    pwd1[0].value = "admin123";
-    pwd1[1].value = "admin123";
-}
-function getUrl(){
-    var aLink = this.getElementByAttrAll('td', 'width', '70%');
-    var retArray = new Array();
-    for (var i = 0; i < aLink.length; i++)
-    {
-        var url = aLink[i].childNodes[1].href;
-        var size = aLink[i].nextElementSibling.childNodes[1].innerHTML;
-        retArray.push(this.appendButton(size, url));
-        aLink[i].appendChild(this.appendButton('download', retArray[i]));
-    }
-    var tdSize = this.getElementByAttrAll('td', 'style', 'text-align:center;white-space:nowrap')
-    console.log(aLink[0].nextElementSibling.childNodes[1].innerHTML);
-    return retArray;
-}
-function getLocked() {
-    var locked = this.getElementByAttr('div', 'class', 'locked');
-    if (locked && (!this.isReply))
-    {
-        console.log(locked)
-        this.isReply = true;
-        var postMsg = document.getElementById('fastpostmessage');
-        postMsg.value = '破隐一击';
-        document.getElementById('fastpostsubmit').click();
-    }
-}
+
 function tapd() {
     let body = document.querySelector('tbody');
+    if (body == null) 
+        return;
+    
+    let isFunction = localStorage.getItem('function');
+    let isFilter = localStorage.getItem('filter');
+    let isCur = localStorage.getItem('curgen');
+    console.log("is:", isFunction, isFilter, isCur);
+
     let children = body.querySelectorAll('tr');
     for (child of children) {
         let aList = child.querySelectorAll('a');
         let state = '';
-        let isRemove = false;
-        for (a of aList) {
-            if (a.getAttribute('id'))
-            {
-                if (a.innerHTML == "已解决" || a.innerHTML == "已实现" || a.innerHTML == "验证中" || a.innerHTML == "测试中" || a.innerHTML == "待测试" || a.innerHTML == "待验收") {
-                    body.removeChild(child);
-                    isRemove = true;
-                    break;
+        if (isFilter == "true"){
+            console.log('im in filter', isFilter);
+            let isRemove = false;
+            for (a of aList) {
+                if (a.getAttribute('id'))
+                {
+                    if (a.innerHTML == "已解决" || a.innerHTML == "已实现" || a.innerHTML == "验证中" || a.innerHTML == "测试中" || a.innerHTML == "待测试" || a.innerHTML == "待验收") {
+                        body.removeChild(child);
+                        isRemove = true;
+                        break;
+                    }
                 }
             }
-        }
-        if (isRemove) {
-            continue;
+            if (isRemove) {
+                continue;
+            }
         }
 
         let gt = child.querySelector("div.growing-title");
         let b = gt.querySelector('a');
         if (b.innerHTML.indexOf("功能点") > 0) {
-            body.removeChild(child);
+            if (isFunction == "true")
+                body.removeChild(child);
         }
     }
 }
@@ -131,35 +92,6 @@ function timerFunc(){
     var that = this;
     var tmpFunc = function(){
         that.tapd();
-        /*
-        if (!that.inputAdd)
-        {
-            var divs = that.getElementByAttrAll('div', 'class', 'input-group');
-            var input = document.createElement('input');
-            input.type = 'text';
-            that.input = input;
-            divs[0].appendChild(input);
-            console.log(divs);
-            that.inputAdd = true;
-        }
-        //aLink[0].childNodes[1].href = '';
-        //aLink[0].childNodes[1].onclick = function(){console.log('123')};
-        //console.log(aLink[0].childNodes[1].href);
-        if (!that.urlAdd)
-        {
-            var btns = that.getUrl();
-            if (btns.length)
-            {
-                that.urlAdd = true;
-                var father = document.getElementById('magnet-table');
-                for (var i = 0; i < btns.length; i++)
-                {
-                    father.appendChild(btns[i]); 
-                }
-            }
-        }*/
-        //console.log(father);
-        //console.log("im here");
     }
     return tmpFunc;
 }
@@ -169,16 +101,36 @@ function setTimer(){
 Insert.prototype.getElementByAttr = getElementByAttr;
 Insert.prototype.getElementByAttrAll = getElementByAttrAll;
 Insert.prototype.setTimer = setTimer;
-Insert.prototype.getTitle = getTitle;
-Insert.prototype.addButton = addButton;
-Insert.prototype.login = login;
 Insert.prototype.appendButton = appendButton;
-Insert.prototype.inputPwd = inputPwd;
 Insert.prototype.timerFunc = timerFunc;
-Insert.prototype.getUrl = getUrl;
-Insert.prototype.getLocked = getLocked;
 
 Insert.prototype.tapd = tapd;
 
 var insert = new Insert();
 insert.setTimer();
+
+function rpc_setFilter(filterName, value, ret) {
+    localStorage.setItem(filterName, value);
+    ret();
+}
+
+function rpc_getLocStorage(name, ret) {
+    console.log("rpc_getLocStorage:", name, ret);
+    let value = localStorage.getItem(name);
+    ret(name, value);
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log('requests is:', request, sendResponse);
+        let callfunc = eval('rpc_' + request.func);
+        let args = request.arg;
+        args.push(function(){
+            let _args = [].slice.call(arguments);
+            sendResponse({
+                rpcId: request.rpcId,
+                args: _args
+            });
+        });
+        callfunc.apply(null, args);
+});
